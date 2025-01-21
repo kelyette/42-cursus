9140786959d6178a189e974459f7d97c8889bffe
kelyette <66877262+kelyette@users.noreply.github.com>
2025-01-21 15:27:04 +0100

    clean up

--- ft_printf/test/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-shift
-TIMEFORMAT="%3R"
-#( time $* ; ) >> time.log 2>&1
-{
-	{ time (eval $1 2>&3 1>/dev/null); } 3>&2 2>> time.log
-} >> time.log
-v=$1
-echo ${v:0:12} " | " >> time.log
