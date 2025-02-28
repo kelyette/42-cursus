21f812636ccb5533a7411520bf3f785c780ad440
kelyette <66877262+kelyette@users.noreply.github.com>
2025-02-28 16:29:59 +0100

    philosophers: start project

---
+++ philosophers/philo/include/philosophers.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcsajka <kcsajka@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 16:11:47 by kcsajka           #+#    #+#             */
+/*   Updated: 2025/02/28 16:24:08 by kcsajka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <string.h>
+# include <unistd.h>
+
+// Allowed functions: 
+//   memset, printf, malloc, free, write, usleep, gettimeofday
+//   pthread_create, pthread_detach, pthread_join
+//   pthread_mutex_init, pthread_mutex_destroy,
+//   pthread_mutex_lock, pthread_mutex_unlock
+
+#endif
